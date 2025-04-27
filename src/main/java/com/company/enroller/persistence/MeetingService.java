4c622d24f0323a51b2ec05b8cf0873d403827791
Andrij Kostecki <kostecki.andrij@gmail.com>
2025-04-27 15:37:43 +0200

    Updated

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -1,25 +1,62 @@
 package com.company.enroller.persistence;
 
 import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
 @Component("meetingService")
+
 public class MeetingService {
 
-	Session session;
+	DatabaseConnector connector;
+
 
 	public MeetingService() {
-		session = DatabaseConnector.getInstance().getSession();
+		connector = DatabaseConnector.getInstance();
 	}
 
 	public Collection<Meeting> getAll() {
 		String hql = "FROM Meeting";
-		Query query = this.session.createQuery(hql);
+
+		Query query = connector.getSession().createQuery(hql);
 		return query.list();
 	}
+
+	public Meeting getMeeting(Long id) {
+
+		String hql = "FROM Meeting";
+
+		Query query = connector.getSession().createQuery(hql);
+		return (Meeting) query.list().get(Math.toIntExact(id));
+    }
+
+	public Meeting add(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(meeting);
+		transaction.commit();
+		return meeting;
+	}
+
+	public void delete(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(meeting);
+		transaction.commit();
+	}
+
+	public void update(Meeting meeting) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().merge(meeting);
+		transaction.commit();
+	}
+
+
 
 }
